2e70bc0d6b434465c90aba64bade2daf402a9720
Fede <chiellofederico@gmail.com>
2021-03-16 15:26:48 +0100

    collego al css e al js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}} ">
+    <title>@yield('title')</title>
 </head>
 <body>
     @include('partials.header')
-
-
+    <main>@yield('content')</main>
     @include('partials.footer')
-    
+
+    <script src="{{ asset('js/app.js')}} "></script>
 </body>
 </html>
