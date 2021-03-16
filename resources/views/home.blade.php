c7595a29124f1b5d78042b13f6892a4c47641bef
Fede <chiellofederico@gmail.com>
2021-03-16 15:41:32 +0100

    metto del contenuto per controllare se funzionano i collegamenti

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+
+@extends('layouts.app')
+
+@section('title','homepage')
+
+@section('content')
+ <h1>Homepage</h1>
+ <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid sequi amet 
+  consequuntur, optio doloribus odio praesentium mollitia fugiat magni unde, 
+  aspernatur facere! Consequatur maiores, obcaecati optio repellendus eveniet sed quam.</p>
+@endsection
