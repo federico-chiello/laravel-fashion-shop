01b32e1e35b968925fc385aea7a3b11e8682c677
Fede <chiellofederico@gmail.com>
2021-03-16 17:46:39 +0100

    stampo i vari dati della tabella dresses

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,6 +4,14 @@
 
 @section('content')
  <h1>Prodotti</h1>
+ @foreach ($vestiti as $vestito)
+ <h3>ID vestito: {{ $vestito->id }}</h3>
+ <ul>
+    <li>{{ $vestito->marca }}</li>
+    <li>{{ $vestito->genere }}</li>
+    <li>{{ $vestito->taglia }}</li>
+</ul>
+ @endforeach
 
 
 @endsection
